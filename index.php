45a9a0bc0b2c591197af7e4727d02b6d15247312
Wyatt Carss <carss.w@gmail.com>
2016-02-17 11:19:17 -0500

    Hide delete/post from unlogged-users.
    Also tidies the code in index.php a teensy smidge.

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
 
+$logged_in = false;
 if (isset($_SESSION['user'])) {
-  echo "<small>hello, {$_SESSION['user']}! <a href='/logout.php'>logout</a></small>";
+  $username = $_SESSION['user'];
+  $logged_in = true;
+}
+
+if ($logged_in) {
+  echo "<small>hello, {$username}! <a href='/logout.php'>logout</a></small>";
 } else {
   echo "<small>hello, unregistered user! <a href='/login.php'>login</a></small>";
 }
@@ -11,17 +17,24 @@ $db = new PDO('mysql:host=localhost;dbname=blog_app;charset=utf8', 'blog_app_use
 
 echo '<html><body>';
 echo '<h1>bloggggg</h1>';
+
 foreach($db->query('select * from posts') as $row) {
   echo "<h2>${row['title']}</h2><p>${row['body']}</p>";
-  echo "<form method='POST' action='/delete.php'>";
-  echo "  <input type='hidden' name='delete_post_id' value='${row['id']}'>";
-  echo "  <input type='submit' name='submit' value='delete'>";
-  echo "</form>";
+  if ($logged_in) {
+    echo "<form method='POST' action='/delete.php'>";
+    echo "  <input type='hidden' name='delete_post_id' value='${row['id']}'>";
+    echo "  <input type='submit' name='submit' value='delete'>";
+    echo "</form>";
+  }
 }
-echo '<form method="POST" action="/new.php">';
-echo '<input type="text" name="title">';
-echo '<input type="text" name="body">';
-echo '<input type="submit" name="submit" value="submit">';
-echo '</form>';
+
+if ($logged_in) {
+  echo '<form method="POST" action="/new.php">';
+  echo '<input type="text" name="title">';
+  echo '<input type="text" name="body">';
+  echo '<input type="submit" name="submit" value="submit">';
+  echo '</form>';
+}
+
 echo '</body></html>';
 ?>
